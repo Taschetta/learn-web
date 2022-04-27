e91f085fd0cd0ba044e3c4de97d1d583d230c75e
santiago <santiagotaschetta@gmail.com>
2022-04-27 16:08:37 -0300

    renamed example - from mdn-using-custom-elements - to mdn-autonomous-custom-elements added example mdn-external-styles

---
+++ web-components/mdn-external-styles/components.js
@@ -0,0 +1,24 @@
+
+class PopUpInfo extends HTMLElement {
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+
+    const imgSrc = this.hasAttribute('img') ? this.getAttribute('img') : 'img/default.png'
+    const infoContent = this.getAttribute('data-text')
+    
+    this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="/style.css">
+      <span class="wrapper">
+        <span class="icon" tabindex="0">
+          <img src="${imgSrc}">
+        </span>
+        <span class="info">
+          ${infoContent}
+        </span>
+      </span>
+    `
+  }
+}
+
+customElements.define('popup-info', PopUpInfo)
